13c5e1499d5777798ddcf6524e7cf2220c07751d
mrsekut <k.cloudspider@gmail.com>
2021-11-11 15:20:01 +0900

    feat: define posts schema

---
+++ Application/Migration/unmigrated-changes.sql
@@ -0,0 +1,10 @@
+-- This file is created by the IHP Schema Designer.
+-- When you generate a new migration, all changes in this file will be copied into your migration.
+--
+-- Use http://localhost:8001/NewMigration or `new-migration` to generate a new migration.
+--
+-- Learn more about migrations: https://ihp.digitallyinduced.com/Guide/database-migrations.html
+CREATE TABLE posts (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL
+);
--- Application/Schema.sql
+++ Application/Schema.sql
@@ -1,1 +1,6 @@
 -- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
+CREATE TABLE posts (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL
+);
