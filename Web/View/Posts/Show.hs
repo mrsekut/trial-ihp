204cb2f22a2a562712c56666ef8beba7c7b689d0
mrsekut <k.cloudspider@gmail.com>
2021-11-14 17:10:02 +0900

    feat: add mmark

--- Web/View/Posts/Show.hs
+++ Web/View/Posts/Show.hs
@@ -1,4 +1,6 @@
 module Web.View.Posts.Show where
+
+import qualified Text.MMark       as MMark
 import           Web.View.Prelude
 
 data ShowView = ShowView { post :: Post }
@@ -8,10 +10,14 @@ instance View ShowView where
         {breadcrumb}
         <h1>{get #title post}</h1>
         <p>{get #createdAt post |> timeAgo}</p>
-        <div>{get #body post}</div>
+        <div>{get #body post |> renderMarkdown}</div>
     |]
         where
             breadcrumb = renderBreadcrumb
                             [ breadcrumbLink "Posts" PostsAction
                             , breadcrumbText "Show Post"
                             ]
+
+            renderMarkdown text = case text |> MMark.parse "" of
+                Left err -> "Something went wrong"
+                Right markdown -> MMark.render markdown |> tshow |> preEscapedToHtml
